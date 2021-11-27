70f5c7a563e42a5c76b0edfd5135d6e635b129b5
Angga Kurniawan <67410461+anggaxd@users.noreply.github.com>
2021-11-27 19:24:45 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-ext = Extension("ambf", sources=["ambf.py"])
+ext = Extension("ambf", sources=["ambf.c"])
 
 setup(
 	ext_modules=[ext],
