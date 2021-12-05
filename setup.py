4e7ad2e32d8dbd830f8b9f031e7be662c0a796d5
Angga Kurniawan <67410461+anggaxd@users.noreply.github.com>
2021-12-05 15:52:06 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import os
+import os, sys
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
@@ -8,11 +8,11 @@ ext = Extension("ambf", sources=["ambf.c"])
 setup(
 	ext_modules=[ext],
 	cmdclass={"build_ext": build_ext}
-) 
+)
 
 try:
 	os.system("rm -rf build")
 except:
 	pass
 
-print("\n [!] info : install berhasil silakan jalankan script dengan mengetik : \033[0;92mpython run.py\033[0;97m")
+sys.exit("\n [!] info : install berhasil silakan jalankan script dengan mengetik : \033[0;92mpython run.py\033[0;97m")
